53fb2de768372350978d31ecfded055db728bd06
sevenmeasure <sevenmeasure@gmail.com>
2022-10-01 10:43:05 +0100

    added new code

--- src/app.js
+++ src/app.js
@@ -13,30 +13,43 @@ function formatDate(timestamp){
   return `${day} ${hours}:${minutes}`
 }
 
-function displayForecast () {
+function formatDay(timestamp){
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ['Sun', 'Mon', 'Tue','Wed','Thu','Fri','Sat'];
+  return days[day]
+}
+
+function displayForecast(response) { 
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   
   let forecastHtml = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
-  
-  days.forEach(function(day){
+  forecast.forEach(function(forecastDay,index){
+    if (index < 6) {
     forecastHtml = forecastHtml + `<div class="col-2">
-    <div class="weather-forecast-date">Thu</div>
-    <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="" width="42"/>
+    <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+    <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42"/>
     <div class="weather-forecast-temperature">
-       <span class="weather-forecast-temperature-max">18°</span>
-       <span class="weather-forecast-temperature-min">12°</span>
+       <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span>
+       <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
     </div>
   </div>`
+    }
   })
   
- 
   forecastHtml =forecastHtml + `</div>`;
   forecastElement.innerHTML = forecastHtml;
 }
 
+function getForecast(coordinates){
+  let apiKey = "701f06352d61835bc4fc894e7b084629";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
+
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response){
-  console.log(response.data)
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
@@ -59,8 +72,9 @@ function displayTemperature(response){
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
-  celsiusTemperature = response.data.main.temp;
-}
+  getForecast(response.data.coord);
+  
+  }
 
 function search(city){
 let apiKey = "8678ae192384fc42897c60c124159d58";
